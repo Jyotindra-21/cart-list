0c33d0e2a7010f50313d6cdbdce3405973265f5a
jyotindra kushwah <jyotindra2421@gmail.com>
2024-02-21 12:06:21 +0530

    cart list task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Button } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    if (count === 0) {
+      setCount(0);
+    } else {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="left">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+          >
+            <Button variant="contained" onClick={decrement}>
+              <RemoveCircleOutlineIcon />
+            </Button>
+            <h1>{count}</h1>
+            <Button variant="contained" onClick={increment}>
+              <AddCircleOutlineIcon />
+            </Button>
+          </div>
+        </div>
+        <div className="right">
+          <div className="todoList">
+            <h1 style={{textAlign:"center"}}>TODO LIST</h1>
+            <ul>
+              {todos.length === 0 && <h1>Loading...</h1>}
+              {todos.length > 0 &&
+                todos.map((todo) => <li key={todo.id}>{todo.title}</li>)}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
